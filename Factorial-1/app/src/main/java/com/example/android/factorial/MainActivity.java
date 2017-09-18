f852fc2481876947f30e428d19a811b04988a07c
petarkuzmanovski <markeloffa8@gmail.com>
2017-09-18 22:29:24 +0200

    Update2 - proekt 1

--- Factorial-1/app/src/main/java/com/example/android/factorial/MainActivity.java
+++ Factorial-1/app/src/main/java/com/example/android/factorial/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.android.factorial;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int finalValue = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void showAlert (View view) {
+        System.out.println("START");
+
+        EditText txt = (EditText) findViewById(R.id.txtInput);
+        int amount = Integer.valueOf(txt.getText().toString());
+
+        System.out.println(amount);
+
+       for(int x = amount; x>0; x--)
+       {
+        finalValue = finalValue *  x;
+       }
+
+        System.out.println(finalValue);
+
+        AlertDialog.Builder myAlert = new AlertDialog.Builder(this);
+        myAlert.setMessage(finalValue).create();
+        myAlert.show();
+    }
+
 }
