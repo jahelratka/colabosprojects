2bfb226382e301e43f3cd244296f522f8b46dfdf
jahelratka <32075183+jahelratka@users.noreply.github.com>
2017-10-06 03:10:28 +0200

    tenis final 001

--- Tennis-4/app/src/main/java/com/example/android/tennis/SecondActivity.java
+++ Tennis-4/app/src/main/java/com/example/android/tennis/SecondActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import android.widget.Button;
@@ -36,8 +37,8 @@ public class SecondActivity extends AppCompatActivity
     private Handler pointPlayHandler = new Handler();
     private int pointPlayTimerSpeed = 1000;
     private int randomPointRange = 18;
-    private int player1ServiceStr = 3;
-    private int player2ServiceStr = 3;
+    private int player1ServiceStr = 8;
+    private int player2ServiceStr = 8;
     private String player1Points = "0";
     private String player2Points = "0";
     private int player1PointsTieBreak = 0;
@@ -46,13 +47,18 @@ public class SecondActivity extends AppCompatActivity
     private int player2Game = 0;
     private String playerToServe;
     private String pointPlayType = "Regular";
-    private int tmpppp = 0;
+    private int player1Set = 0;
+    private int player2Set = 0;
+    private int setPlayed = 1;
+    private int tieBreakPontPlayed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        playerToServe = "PLAYER1";
 
         lblPlayer1points = (TextView) findViewById(R.id.lblPlayer1points);
         lblPlayer2points = (TextView) findViewById(R.id.lblPlayer2points);
@@ -80,7 +86,9 @@ public class SecondActivity extends AppCompatActivity
         lblPlayerToServe = (TextView) findViewById(R.id.lblPlayerToServe);
         pointPlayHandler.postDelayed(pointPlayrun, pointPlayTimerSpeed);
         pauseButton = (Button) findViewById(R.id.pauseButton);
-        playerToServe = "PLAYER1";
+
+        lblPlayerToServe.setText("SET "+Integer.toString(setPlayed)+"\nPLAYER 1 SERVING\n"+pointPlayType);
+
     }
 
     void btnBackClick(View v)
@@ -140,7 +148,7 @@ public class SecondActivity extends AppCompatActivity
                 endedRegularPoint("payer1");
             } else if (player2PointChance > player1PointChance) {
                 endedRegularPoint("payer2");
-            }else {
+            }else if (player2PointChance == player1PointChance) {
                 switch (playerToServe){
                     case "PLAYER1":
                         endedRegularPoint("payer1");
@@ -155,7 +163,7 @@ public class SecondActivity extends AppCompatActivity
                 endedTieBreakPoint("payer1");
             } else if (player2PointChance > player1PointChance) {
                 endedTieBreakPoint("payer2");
-            }else {
+            }else if (player2PointChance == player1PointChance) {
                 switch (playerToServe){
                     case "PLAYER1":
                         endedTieBreakPoint("payer1");
@@ -176,6 +184,7 @@ public class SecondActivity extends AppCompatActivity
     }
 
     private void endedTieBreakPoint(String payerWon) {
+        Log.d("point won - ",payerWon);
         switch(payerWon){
             case "payer1":
                 player1PointsTieBreak++;
@@ -184,15 +193,50 @@ public class SecondActivity extends AppCompatActivity
                 player2PointsTieBreak++;
                 break;
         }
-        if((player1PointsTieBreak - player2PointsTieBreak) >= 2){
-
+        tieBreakPontPlayed++;
+        Log.d("serving - ",playerToServe);
+        Log.d("P1 Tie - ",Integer.toString(player1PointsTieBreak));
+        Log.d("P2 Tie - ",Integer.toString(player2PointsTieBreak));
+        if( (player1PointsTieBreak + player2PointsTieBreak) == 1){
+            //FORCE SERVE CHANGE AFTER FIRST TIE BREAK POINT
+            tieBreakPontPlayed++;
         }
-        if((player2PointsTieBreak - player1PointsTieBreak) >=2){
-
+        if(playerToServe.equals("PLAYER1") && tieBreakPontPlayed == 2){
+            lblPlayerToServe.setText("SET "+Integer.toString(setPlayed)+"\nPLAYER 2 SERVING\n"+pointPlayType);
+            player2ToServe.setChecked(true);
+            player1ToServe.setChecked(false);
+            playerToServe = "PLAYER2";
+            tieBreakPontPlayed = 0;
         }
+        if(playerToServe.equals("PLAYER2") && tieBreakPontPlayed == 2){
+            lblPlayerToServe.setText("SET "+Integer.toString(setPlayed)+"\nPLAYER 1 SERVING\n"+pointPlayType);
+            player1ToServe.setChecked(true);
+            player2ToServe.setChecked(false);
+            playerToServe = "PLAYER1";
+            tieBreakPontPlayed = 0;
+        }
+        if((player1PointsTieBreak - player2PointsTieBreak) >= 2 && player1PointsTieBreak  >=7){
+            endedGame("payer1");
+            player1PointsTieBreak = 0;
+            player2PointsTieBreak = 0;
+            player1Points = "0";
+            player2Points = "0";
+            tieBreakPontPlayed = 0;
+        }
+        if((player2PointsTieBreak - player1PointsTieBreak) >=2 && player2PointsTieBreak  >=7){
+            endedGame("payer2");
+            player1PointsTieBreak = 0;
+            player2PointsTieBreak = 0;
+            player1Points = "0";
+            player2Points = "0";
+            tieBreakPontPlayed = 0;
+        }
+        lblPlayer1points.setText(Integer.toString(player1PointsTieBreak));
+        lblPlayer2points.setText(Integer.toString(player2PointsTieBreak));
     }
 
     private void endedRegularPoint(String payerWon) {
+        Log.d("point won - ",payerWon);
         switch(payerWon){
             case "payer1":
                 switch(player1Points){
@@ -211,10 +255,10 @@ public class SecondActivity extends AppCompatActivity
                         }else{
                             player1Points = "Deuce";
                             player2Points = "Deuce";
+                            Log.d("Deuce - ",payerWon);
                             animateText(lblPlayer1points);
                             animateText(lblPlayer2points);
                         }
-
                         break;
                     case "40":
                         endedGame("payer1");
@@ -222,15 +266,18 @@ public class SecondActivity extends AppCompatActivity
                     case "Deuce":
                         player1Points = "A";
                         player2Points = "";
+                        Log.d("Advantage - ",payerWon);
                         animateText(lblPlayer1points);
                         break;
                     case "":
                         player1Points = "Deuce";
                         player2Points = "Deuce";
+                        Log.d("Deuce - ",payerWon);
                         animateText(lblPlayer1points);
                         animateText(lblPlayer2points);
                         break;
                     case "A":
+                        Log.d("WON - ",payerWon);
                         endedGame("payer1");
                         break;
                 }
@@ -252,10 +299,10 @@ public class SecondActivity extends AppCompatActivity
                         }else{
                             player2Points = "Deuce";
                             player1Points = "Deuce";
+                            Log.d("Deuce - ",payerWon);
                             animateText(lblPlayer1points);
                             animateText(lblPlayer2points);
                         }
-
                         break;
                     case "40":
                         endedGame("payer2");
@@ -263,25 +310,31 @@ public class SecondActivity extends AppCompatActivity
                     case "Deuce":
                         player2Points = "A";
                         player1Points = "";
+                        Log.d("Advantage - ",payerWon);
                         animateText(lblPlayer2points);
                         break;
                     case "":
                         player1Points = "Deuce";
                         player2Points = "Deuce";
+                        Log.d("Deuce - ",payerWon);
                         animateText(lblPlayer1points);
                         animateText(lblPlayer2points);
+                        break;
                     case "A":
+                        Log.d("WON - ",payerWon);
                         endedGame("payer2");
                         break;
                 }
                 break;
         }
+
         lblPlayer1points.setText(player1Points);
         lblPlayer2points.setText(player2Points);
 
     }
 
     private void endedGame(String payerWonGame) {
+        Log.d("game won - ",payerWonGame);
         switch(payerWonGame){
             case "payer1":
                 player1Game++;
@@ -292,34 +345,94 @@ public class SecondActivity extends AppCompatActivity
         }
         player1Points = "0";
         player2Points = "0";
-        if(!lblset1Player1Games.getText().equals(Integer.toString(player1Game))){
-            animateText(lblset1Player1Games);
+        player1PointsTieBreak = 0;
+        player2PointsTieBreak = 0;
+
+        TextView[] lblSetPlayer1Array = {lblset1Player1Games, lblset2Player1Games, lblset3Player1Games, lblset4Player1Games, lblset5Player1Games};
+        TextView[] lblSetPlayer2Array = {lblset1Player2Games, lblset2Player2Games, lblset3Player2Games, lblset4Player2Games, lblset5Player2Games};
+
+        if(!lblSetPlayer1Array[setPlayed-1].getText().equals(Integer.toString(player1Game))){
+            lblSetPlayer1Array[setPlayed-1].setText(Integer.toString(player1Game));
+            animateText(lblSetPlayer1Array[setPlayed-1]);
         }
-        if(!lblset1Player2Games.getText().equals(Integer.toString(player2Game))){
-            animateText(lblset1Player2Games);
+        if(!lblSetPlayer2Array[setPlayed-1].getText().equals(Integer.toString(player2Game))){
+            lblSetPlayer2Array[setPlayed-1].setText(Integer.toString(player2Game));
+            animateText(lblSetPlayer2Array[setPlayed-1]);
         }
-        lblset1Player1Games.setText(Integer.toString(player1Game));
-        lblset1Player2Games.setText(Integer.toString(player2Game));
-        if(playerToServe.equals("PLAYER1")){
-            lblPlayerToServe.setText("PLAYER 2 SERVING");
-            player2ToServe.setChecked(true);
-            player1ToServe.setChecked(false);
-            playerToServe = "PLAYER2";
-        }else{
-            lblPlayerToServe.setText("PLAYER 1 SERVING");
-            player1ToServe.setChecked(true);
-            player2ToServe.setChecked(false);
+
+
+        if(player1Game >= 6 ||  player2Game >= 6 ){
+            if(player1Game == 6 && player2Game == 6){
+                pointPlayType = "TieBreak";
+                if((setPlayed%2) == 0 ){
+                    playerToServe = "PLAYER1";
+                }else{
+                    playerToServe = "PLAYER2";
+                }
+            }else if(pointPlayType.equals("TieBreak")){
+                endedSet(payerWonGame);
+            }else{
+                if((player1Game - player2Game) >= 2){
+                    endedSet("payer1");
+                }
+                if((player2Game - player1Game) >= 2){
+                    endedSet("payer2");
+                }
+            }
+
+        }
+        if(player1Set < 3 &&player2Set < 3) {
+            if (playerToServe.equals("PLAYER1")) {
+                lblPlayerToServe.setText("SET "+Integer.toString(setPlayed)+"\nPLAYER 2 SERVING\n"+pointPlayType);
+                player2ToServe.setChecked(true);
+                player1ToServe.setChecked(false);
+                playerToServe = "PLAYER2";
+            } else {
+                lblPlayerToServe.setText("SET "+Integer.toString(setPlayed)+"\nPLAYER 1 SERVING\n"+pointPlayType);
+                player1ToServe.setChecked(true);
+                player2ToServe.setChecked(false);
+                playerToServe = "PLAYER1";
+            }
+        }
+    }
+
+    private void endedSet(String playerWonSet) {
+        switch(playerWonSet){
+            case "payer1":
+                player1Set++;
+                break;
+            case "payer2":
+                player2Set++;
+                break;
+        }
+        setPlayed++;
+        player1Game = 0;
+        player2Game = 0;
+        if(player1Set == 3){
+            lblPlayerToServe.setText("PLAYER 1 WON");
+            pointPlayHandler.removeCallbacks(pointPlayrun);
+            animateText(lblPlayerToServe);
+        }
+        if(player2Set == 3){
+            lblPlayerToServe.setText("PLAYER 2 WON");
+            pointPlayHandler.removeCallbacks(pointPlayrun);
+            animateText(lblPlayerToServe);
+        }
+        if((setPlayed%2) == 0 ){
             playerToServe = "PLAYER1";
+        }else{
+            playerToServe = "PLAYER2";
         }
+        pointPlayType = "Regular";
     }
 
     private Runnable pointPlayrun = new Runnable() {
         @Override
         public void run() {
-      /* do what you need to do */
-            pointPlayedFunction();
-      /* and pointPlayedFunction comes the "trick" */
-            pointPlayHandler.postDelayed(this, pointPlayTimerSpeed);
+            if(player1Set < 3 &&player2Set < 3) {
+                pointPlayedFunction();
+                pointPlayHandler.postDelayed(this, pointPlayTimerSpeed);
+            }
         }
     };
 }
